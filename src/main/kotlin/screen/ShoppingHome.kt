59b5f0961a9b109a71958bf515bd1cf8942ac628
ijimin <zhzhwlals12@gmail.com>
2022-07-23 00:38:32 +0900

    실습 3-2. Home화면과 Category 화면으로 분리

--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -6,12 +6,6 @@ class ShoppingHome {
         showWelcomeMessage()
         showCategories()
     }
-
-    private fun showCategories() {
-        val shoppingCategory = ShoppingCategory()
-        shoppingCategory.showCategories()
-    }
-
     private fun showWelcomeMessage() {
         println("안녕하세요. picobell에 오신 것을 환영합니다!")
         println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
@@ -23,6 +17,11 @@ class ShoppingHome {
             원하시는 카테고리를 입력해주세요.
         """.trimIndent()
         )
+    }
+
+    private fun showCategories() {
+        val shoppingCategory = ShoppingCategory()
+        shoppingCategory.showCategories()
     }
 }
 
