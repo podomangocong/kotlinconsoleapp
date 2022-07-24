d9fb801195458b59d1a5156b9b705a14db781072
ijimin <zhzhwlals12@gmail.com>
2022-07-24 21:38:00 +0900

    실습 4-1. 장바구니에 추가한 상품은 싱글톤 CartItems가 관리하고 ShoppingCart 화면과 역할 분리

--- src/main/kotlin/screen/ShoppingCategory.kt
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -15,7 +15,8 @@ class ShoppingCategory {
             selectedCategory = readLine() // 입력하라고 재요청
         }
         if (selectedCategory == "#") {
-            // TODO 1. 장바구니 이동
+            val shoppingCart = ShoppingCart()
+            shoppingCart.showCartItems()
         } else {
             if (categories.contains(selectedCategory)) {
                 val shoppingProductList = ShoppingProductList()
